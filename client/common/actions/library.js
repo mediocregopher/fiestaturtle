6ae683af0dcfa48486160d9c560f7e0310d229f2
Marco Munizaga <marco@marcopolo.io>
2016-02-28 07:58:02 -0800

    Start rpc hookup

--- client/common/actions/library.js
+++ client/common/actions/library.js
@@ -62,6 +62,41 @@ export function playPlaylist (playlist: Playlist): TypedAsyncAction<DeletePlayli
   }
 }
 
+export function getUser (): any {
+  return dispatch => {
+    rpc('GetUser', {}).then(({error, result}) => {
+      if (error) {
+        console.error("Error fetching user:", error)
+      } else {
+        console.log('user result:', result)
+      }
+    })
+  }
+}
+
+function rpc (method: string, param: Object) {
+  return jsonRPC( {"jsonrpc": "2.0", "method": "Brian." + method, "params": param, "id": 1})
+    .then(r => r.json())
+}
+
+function jsonRPC (body: any) {
+  const msg = JSON.stringify({
+    ...body
+  })
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json")
+  myHeaders.append('Access-Control-Allow-Origin', '*')
+  return fetch(
+    'http://localhost:4567/rpc',
+    {
+      headers: myHeaders,
+      method: 'POST',
+      body: msg
+    }
+  )
+}
+
+window.rpc = rpc
 
 // TODO delete playlist from brian and create it again
 function createPlaylistCall (playlist: Playlist): Promise {
